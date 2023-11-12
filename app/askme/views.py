d3ac3f05fede822b3237989dcc0abeeced1abd55
IlyaMerzlikin <atomchannle@gmail.com>
2023-11-13 01:43:08 +0300

    Created Urls and Views also changed settings

--- app/askme/views.py
+++ app/askme/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def settings(request):
+    return render(request, 'settings.html')
+
+
+def question(request):
+    return render(request, 'question.html')
+
+
+def tag(request):
+    return render(request, 'tag.html')
+
+
+def hot(request):
+    return render(request, 'index.html')
+
